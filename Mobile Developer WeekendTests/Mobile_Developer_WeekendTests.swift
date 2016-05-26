87679892a19031bf1963a2a92088ebb3bd1c98e9
repixels <ehab.m.ashraf@repixels.co>
2016-05-26 17:49:18 +0200

    Initial Commit

---
+++ Mobile Developer WeekendTests/Mobile_Developer_WeekendTests.swift
@@ -0,0 +1,36 @@
+//
+//  Mobile_Developer_WeekendTests.swift
+//  Mobile Developer WeekendTests
+//
+//  Created by RE Pixels on 5/26/16.
+//  Copyright © 2016 ITI. All rights reserved.
+//
+
+import XCTest
+@testable import Mobile_Developer_Weekend
+
+class Mobile_Developer_WeekendTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
