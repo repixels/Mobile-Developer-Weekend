675e3c21db029e96b0abb266bc6d04b98f05a1dd
A-Hamdy <ahmedhamdy.fcih2011@gmail.com>
2016-06-03 19:19:14 +0200

    Test User Dao

--- Mobile Developer Weekend/NetWork_Manager.swift
+++ Mobile Developer Weekend/NetWork_Manager.swift
@@ -27,33 +27,33 @@ class NetWork_Manager: NSObject {
             //  var json : NSObject
             do
             {
-                let  json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
-                /// for loop
-                //  let dic = json [0]
-                var counter :Int = json.count
-                for i : Int in 1..<counter {
-                    let dic = json [i]
-                    //
-                    //    let title : String? = dic.objectForKey("title") as? String
-                    //    let imageURL : String? = dic.objectForKey("image") as? String
-                    //    let rating : Float? = dic.objectForKey("rating") as? Float
-                    //    let releaseYear : Int? = dic.objectForKey("releaseYear") as? Int
-                    //    let genre : [String]? = dic.objectForKey("genre") as? [String]
-                    //
-                    //   var movie1: Movie = Movie(title: title, image: imageURL, rating:rating , releaseYear: releaseYear, genre: genre!)
-                    // NSData * imageData = initWiithContentOFURL
-                    //UIImage imageWithData
-                    // 3lshan 2gyb main thread bt3ty
-                    //     var json = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments) as NSArray
-                    //    print(json)
-                    //    self.array.append(movie1)
-                    //dispatch_async(dispatch_get_main_queue(), {
-                        //   self.myLabel.text = title!
-                        //  self.loader.stopAnimating()
-                        //self.tableView.reloadData()
-                        // UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-                  //  })
-              }
+//                let  json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+//                /// for loop
+//                //  let dic = json [0]
+//                var counter :Int = json.count
+//                for i : Int in 1..<counter {
+//                    let dic = json [i]
+//                    //
+//                    //    let title : String? = dic.objectForKey("title") as? String
+//                    //    let imageURL : String? = dic.objectForKey("image") as? String
+//                    //    let rating : Float? = dic.objectForKey("rating") as? Float
+//                    //    let releaseYear : Int? = dic.objectForKey("releaseYear") as? Int
+//                    //    let genre : [String]? = dic.objectForKey("genre") as? [String]
+//                    //
+//                    //   var movie1: Movie = Movie(title: title, image: imageURL, rating:rating , releaseYear: releaseYear, genre: genre!)
+//                    // NSData * imageData = initWiithContentOFURL
+//                    //UIImage imageWithData
+//                    // 3lshan 2gyb main thread bt3ty
+//                    //     var json = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments) as NSArray
+//                    //    print(json)
+//                    //    self.array.append(movie1)
+//                    //dispatch_async(dispatch_get_main_queue(), {
+//                        //   self.myLabel.text = title!
+//                        //  self.loader.stopAnimating()
+//                        //self.tableView.reloadData()
+//                        // UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+//                  //  })
+//              }
                 
             } catch {
                 print("Error")
